7f729ec3fd529442c94bf20b859938edc0e5d7f6
Paul Goldsmith-Pinkham <paulgp@gmail.com>
2021-03-12 21:41:12 -0500

    Duration Lecture

--- lectures/lecture11_prep.do
+++ lectures/lecture11_prep.do
@@ -4,13 +4,14 @@ use "/Users/psg24/Dropbox/Papers/GGP_HoldingLength/data/holding_data.dta", clear
 
 tsset property_id transaction_t
 
+
 gen fym = F.ym
 replace fym = ym(2017,9) if fym == .
 gen duration  = fym - ym
 keep if duration > 0
 
 gen year = year(dofm(ym))
-sample .01,  by(year)
+sample 1,  by(year)
 
 
 save "/Users/psg24/Dropbox/Papers/GGP_HoldingLength/data/holding_data_sample100k.dta", replace
--- lectures/likelihood_3.R
+++ lectures/likelihood_3.R
@@ -7,6 +7,7 @@ library(estimatr)
 library(haven)
 
 dblue = rgb(0, 114, 178, max = 255)
+dred = rgb(213,94,0, max = 255)
 acs_data <- read_csv("~/Dropbox/Papers/GGP_HoldingLength/data/usa_00014.csv")
 acs_data <- usa_00014
 corelogic_data = read_dta("~/Dropbox/Papers/GGP_HoldingLength/data/holding_data_sample100k.dta")
@@ -27,6 +28,108 @@ ggplot(data = corelogic_data %>% filter(!is.na(duration))) +
   theme(text = element_text(size=24)) +
   xlim(0,30)
 ggsave("images/housing_duration_full.png")
+
+ggplot(data = corelogic_data %>% filter(!is.na(duration)),
+       aes(x = duration/12)) +
+  geom_step(aes(y = 1 - ..y..), stat='ecdf') +
+  labs(x = "Duration in years",
+       y = "",
+       subtitle = "Survival probability") +
+  theme_minimal() +
+  theme(text = element_text(size=24)) +
+  xlim(0,30)
+
+ggsave("images/housing_duration_full_naive_survival.png")
+
+library(survminer)
+library(condsurv)
+library(survival)
+model = survfit(Surv(time, status) ~ 1, 
+                data = corelogic_data %>% filter(!is.na(duration)) %>%
+                  mutate(time = duration/12) %>%
+                  mutate(status = case_when(fym == 692 ~ 1,
+                                            TRUE ~ 2) ))
+ggplot(data = corelogic_data %>% filter(!is.na(duration)),
+       aes(x = duration/12)) +
+  geom_step(aes(y = 1 - ..y..), stat='ecdf', color = dred) +
+  labs(x = "Duration in years",
+       y = "",
+       subtitle = "Survival probability") +
+  theme_minimal() +
+  theme(text = element_text(size=24)) +
+  xlim(0,30) + 
+  geom_step(data = tibble(y = model$surv, x = model$time), 
+            aes(y = y, x = x), color=dblue)  +
+  annotate("text", x = 25, y = 0.38, 
+           hjust = 0, size = 7, color = dblue, 
+           label = "Kaplan-Meier")  +
+  annotate("text", x = 25, y = 0.05, 
+           hjust = 0, size = 7, 
+           color = dred, label = "Unadjusted")
+
+ggsave("images/housing_duration_full_naive_survival_v_KM.png")
+
+model_boom = survfit(Surv(time, status) ~ 1, 
+                data = corelogic_data %>% filter(!is.na(duration)) %>%
+                  mutate(time = duration/12) %>%
+                  mutate(status = case_when(fym == 692 ~ 1,
+                                            TRUE ~ 2) ) %>%
+                  filter(year > 2000 & year < 2008))
+model_bust = survfit(Surv(time, status) ~ 1, 
+                      data = corelogic_data %>% filter(!is.na(duration)) %>%
+                        mutate(time = duration/12) %>%
+                        mutate(status = case_when(fym == 692 ~ 1,
+                                                  TRUE ~ 2) )%>%
+                       filter(year > 2007 & year < 2011))
+ggplot(data = corelogic_data %>% filter(!is.na(duration)),
+       aes(x = duration/12)) +
+  labs(x = "Duration in years",
+       y = "",
+       subtitle = "Survival probability") +
+  theme_minimal() +
+  theme(text = element_text(size=24)) +
+  xlim(0,18) + 
+  geom_step(data = tibble(y = model_boom$surv, x = model_boom$time), 
+            aes(y = y, x = x), color=dblue)  +
+  geom_step(data = tibble(y = model_bust$surv, x = model_bust$time), 
+            aes(y = y, x = x), color=dred)  +
+  annotate("text", x = 15, y = 0.48, 
+           hjust = 0, size = 7, color = dblue, 
+           label = "Boom - 2000-2006")  +
+  annotate("text", x = 10, y = 0.64, 
+           hjust = 0, size = 7, 
+           color = dred, label = "Bust - 2007-2010")
+ggsave("images/housing_duration_survival_KM_boom_bust.png")
+
+boom_data = tibble(y = model_boom$surv, x = model_boom$time) %>%
+  mutate(f = lag(y)-y) %>%
+  mutate(f = case_when(is.na(f) ~ 1-y,
+                       TRUE ~ f)) %>%
+  mutate(h = f/y)
+bust_data = tibble(y = model_bust$surv, x = model_bust$time) %>%
+  mutate(f = lag(y)-y) %>%
+  mutate(f = case_when(is.na(f) ~ 1-y,
+                       TRUE ~ f)) %>%
+  mutate(h = f/y)
+ggplot(data = corelogic_data %>% filter(!is.na(duration)),
+       aes(x = duration/12)) +
+  labs(x = "Duration in years",
+       y = "",
+       subtitle = "Hazard rate") +
+  theme_minimal() +
+  theme(text = element_text(size=24)) +
+  xlim(0,18) + 
+  geom_step(data = boom_data, 
+            aes(y = h, x = x), color=dblue)  +
+  geom_step(data = bust_data, 
+            aes(y = h, x = x), color=dred)  +
+  annotate("text", x = 15, y = 0.0, 
+           hjust = 0, size = 7, color = dblue, 
+           label = "Boom - 2000-2006")  +
+  annotate("text", x = 10, y = 0.0, 
+           hjust = 0, size = 7, 
+           color = dred, label = "Bust - 2007-2010")
+ggsave("images/housing_duration_hazard_KM_boom_bust.png")
 
 
 ggplot(data = corelogic_data %>% filter(!is.na(duration) & year >= 2010)) +
