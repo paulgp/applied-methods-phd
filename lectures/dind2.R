17c9f2db7f75f41045c9121b407131f90607fc1f
Paul Goldsmith-Pinkham <paulgp@gmail.com>
2021-03-23 13:02:29 -0400

    Synth

---
+++ lectures/dind2.R
@@ -0,0 +1,13 @@
+
+install.packages("Synth") 
+library(tidyverse)
+library(Synth)
+devtools::install_github("synth-inference/synthdid")
+library(synthdid)
+
+
+
+
+dblue = rgb(0, 114, 178, max = 255)
+dred = rgb(213,94,0, max = 255)
+
